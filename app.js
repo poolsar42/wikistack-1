38fcbb6584bad6703590b8697c0081628c1d7693
poolsar42 <poolsar.42@gmail.com>
2021-08-12 23:24:05 +0300

    Sequilize Add

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const { db, Page, User } = require("./models");
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended:false }));
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+app.get("/", async (req, res) => {
+  res.send("hello world");
+});
 
-const PORT = 1337;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`)
-})
+const init = async () => {
+  const PORT = 1337;
+  await db.sync({ force: true });
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
